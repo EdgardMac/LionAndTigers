88b817658fe08954a9675d339d18107f7daa3824
Edgard Maciel <Edgard@Black-Beauty.local>
2015-10-02 16:29:32 -0500

    Primera Entrega

--- Lions&Tigers/ViewController.swift
+++ Lions&Tigers/ViewController.swift
@@ -9,16 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
 
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage( named: "BengalTiger.jpg")
+        
+        print("My tiger's name is: \(myTiger.name), it's age is: \(myTiger.age), it's breed is: \(myTiger.breed), and it's image is: \(myTiger.image)")
+        
+        myImageView.image=myTiger.image
+        nameLabel.text=myTiger.name
+        ageLabel.text="\(myTiger.age)"
+        breedLabel.text=myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigres"
+        secondTiger.age = 2
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger ()
+
+        thirdTiger.name = "Jacob"
+        thirdTiger.age = 4
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
+    
 
 
 }
---
+++ Lions&Tigers/tiger.swift
@@ -0,0 +1,20 @@
+//
+//  tiger.swift
+//  Lions&Tigers
+//
+//  Created by Edgard Maciel on 10/2/15.
+//  Copyright © 2015 Edgard Maciel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+    
+
+}
